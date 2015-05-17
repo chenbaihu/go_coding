34f86950ebeebbad89ddfee2f7b7b123b6fce086
xychenbaihu@163.com <xychenbaihu@163.com>
2015-05-17 15:53:47 +0800

    add const.go

--- src/const.go
+++ src/const.go
@@ -15,6 +15,13 @@ func main() {
 	fmt.Printf("%d\n", kFAIL)
 
 	const (
+	         Type1    int = 0                               
+		 Type2    int = 2                               
+		 Type3    int = 4                               
+		 Type4    int = Type2|Type3 
+	)
+
+	const (
 		str1 = "111111"
 		str2 = "ccccccc"
 		str3 = "dddddddd"
