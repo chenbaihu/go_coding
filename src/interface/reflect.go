85893b0c9b3de55234116da05fc6c17d96154dfd
xychenbaihu@163.com <xychenbaihu@163.com>
2015-06-29 21:16:27 +0800

    add reflect.go

--- src/interface/reflect.go
+++ src/interface/reflect.go
@@ -14,27 +14,36 @@ func show(i interface{}) {
 	switch i.(type) {
 	case *Person:
 
-		t1 := reflect.TypeOf(i) //得到类型的元数据
+		//得到实际的值(方法1)
+		v1 := reflect.ValueOf((i).(*Person))
+		fmt.Printf("reflect.ValueOf v1=%v\n", v1)
+
+		//得到实际的值(方法2)
+		v2 := reflect.ValueOf(i)
+		fmt.Printf("reflect.ValueOf v2=%v\n", v2)
+
+		if v1 == v2 {
+			fmt.Printf("v1==v2\n")
+		}
+
+		//得到类型的元数据(方法1)
+		t1 := reflect.TypeOf(i)
 		fmt.Println("reflect.TypeOf t1=", t1)
 
-		//v := reflect.ValueOf((i).(*Person)) //得到实际的值
-		v := reflect.ValueOf(i) //得到实际的值
-		fmt.Printf("reflect.ValueOf v=%v\n", v)
-
-		myref := v.Elem()
-		t2 := myref.Type() //得到类型的元数据
-		fmt.Println("reflect.TypeOf t2:", t2)
+		//得到类型的元数据(方法2)
+		myref := v2.Elem()
+		t2 := myref.Type()
+		fmt.Println("reflect.TypeOf t2=", t2)
 
 		for i := 0; i < myref.NumField(); i++ {
 			fmt.Printf("i=%d\n", i)
-			fields := myref.Field(i).String() // name := v.Elem().Field(0).String()
+			fields := myref.Field(i).String() // name := v2.Elem().Field(0).String()
 			fmt.Printf("i=%d\tfields=%s\n", i, fields)
 
-			field := myref.Field(i) // name := v.Elem().Field(0)
+			field := myref.Field(i) // name := v2.Elem().Field(0)
 			//fmt.Printf("%d. %s %s = %v \n", i, t2.Field(i).Name, field.Type(), field.Interface())
 			fmt.Printf("i=%d\tname=%s\ttype=%s\tfields=%s\n", i, t2.Field(i).Name, field.Type(), field.String())
 		}
-
 	}
 }
 
