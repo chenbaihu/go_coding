aac84d58de17a7e5c31d1e003bd8b24da933eacb
chenbaihu <chenbaihu@didichuxing.com>
2016-04-20 15:37:54 +0800

    change net/util/getipaddress.go

---
+++ src/net/util/getipaddress.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+    "errors"
+    "net"
+)
+
+func GetLocalIPv4Address() (string, error) {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return "", err 
+    }   
+
+    for _, a := range addrs {
+        if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                return ipnet.IP.String(), nil 
+            }   
+        }   
+    }   
+
+    return "", errors.New("no ipv4 address found!")
+}
+
