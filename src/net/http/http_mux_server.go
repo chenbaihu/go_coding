848541e63bd157f9b9a7dd886473c827cd84123c
chenbaihu <chenbaihu@didichuxing.com>
2016-04-25 18:34:04 +0800

    change http_mux_server.go

---
+++ src/net/http/http_mux_server.go
@@ -0,0 +1,97 @@
+/*
+   1、启动work协程池
+   2、启动http服务
+*/
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	_ "os"
+	"runtime"
+	"runtime/debug"
+	"strings"
+	_ "sync"
+)
+
+type DataInfo struct {
+	Logid string
+	Cmd   string
+	Data  string
+	Rsp   *http.ResponseWriter
+}
+
+var reqMapChan = make(chan *DataInfo, 1000000)
+
+type HttpHandler struct {
+	Name     string
+	Callfunc func(w http.ResponseWriter, r *http.Request, logid string)
+}
+
+func (hh *HttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	remoteaddr := strings.Split(r.RemoteAddr, ":")
+	remoteip := remoteaddr[0]
+	inputIp := r.Header.Get("X-Forwarded-For")
+	if inputIp == "" {
+		inputIp = remoteip
+	}
+
+	//sid := r.URL.Query().Get("sid")
+
+	logid := "logid"
+
+	hh.Callfunc(w, r, logid)
+}
+
+func handle_kv_multi_set(w http.ResponseWriter, r *http.Request, logid string) {
+	data := r.FormValue("data")
+	req := &DataInfo{Logid: logid, Cmd: "mset", Data: data, Rsp: &w}
+	reqMapChan <- req
+	io.WriteString(w, "req process ok--------------")
+}
+
+func handle_kv_multi_get(w http.ResponseWriter, r *http.Request, logid string) {
+	data := r.FormValue("data")
+	req := &DataInfo{Logid: logid, Cmd: "mget", Data: data, Rsp: &w}
+	reqMapChan <- req
+	io.WriteString(w, "req process ok--------------")
+}
+
+func MapWorker() {
+	for {
+		select {
+		case data := <-reqMapChan:
+			//dealworkdata(data)
+			fmt.Printf("data=%v\n", data)
+			//io.WriteString(*data.Rsp, "req process ok--------------")
+		}
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// set recover
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("abort, unknown error, reason:%v, stack:%s\n",
+				err, string(debug.Stack()))
+		}
+	}()
+
+	// start woker pool
+	for i := 0; i < 5; i++ {
+		go MapWorker()
+	}
+
+	// start http server
+	mux := http.NewServeMux()
+
+	//kv相关特征操作接口
+	mux.Handle("/kv/multi_get", &HttpHandler{Name: "handle_kv_multi_get", Callfunc: handle_kv_multi_get})
+	mux.Handle("/kv/multi_set", &HttpHandler{Name: "handle_kv_multi_set", Callfunc: handle_kv_multi_set})
+
+	err := http.ListenAndServe(":12345", mux)
+	fmt.Printf("http listen fail: %s\n", err.Error())
+}
